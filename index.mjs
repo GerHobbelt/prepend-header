58503166c338c7eef6e3c120db9be04709337b15
Ger Hobbelt <ger@hobbelt.com>
2021-01-29 20:16:13 +0100

    removed debugging statements. Also fixed bug in not properly handling the async prependFile() API: this one returns a Promise, which we must handle, NOT a callback!

--- index.mjs
+++ index.mjs
@@ -18,6 +18,7 @@ import { flatten, readFilePromise } from './utils.mjs';
 const flatArgs = flatten(Object.values(args));
 const pkg = JSON.parse(fs.readFileSync(path.normalize(path.join(__dirname, 'package.json')), 'utf8'));
 
+
 if (!flatArgs.length) {
   console.log(
     `
@@ -39,7 +40,6 @@ async function loadHeader(headerPath) {
   try {
     h = await import(headerPath);
   } catch (e) {
-    console.log(`Unable to import module ${headerPath}`, e);
     throw new ImportError(`Unable to import module ${headerPath}: ${e}`);
   }
   return h;
@@ -50,65 +50,45 @@ function importHeaderJS(headerPath) {
     if (path.isAbsolute(headerPath)) {
       headerPath = path.normalize(headerPath).replace(/[\\]/g, '/');
       loadHeader('file://' + headerPath).then((data, err) => {
-        console.log('loadHeader ->', {headerPath, data, err});
         if (err) {
           p = `${appPath}/header.config.mjs`;
           p = path.normalize(p).replace(/[\\]/g, '/');
           loadHeader('file://' + p).then((data2, err2) => {
-            console.log('loadHeader ->', {p, data2, err2});
             if (err2) {
-              console.log(`Unable to prepend, because no valid header javascript file in app root or passed as path. Argument passed: ${headerPath}`);
-              //process.exit(1);
               reject(err);   // reject with original error
             } else {
               resolve(data2);
             }
           }).catch((err) => {
-            console.error('headerPromise error', err)
             reject(err);
-          }).finally(() => {
-            console.error('headerPromise final.')
           });
         } else {
           resolve(data);
         }
       }).catch((err) => {
-        console.error('headerPromise error', err)
         reject(err);
-      }).finally(() => {
-        console.error('headerPromise final.')
       });
     } else {
       let p = path.join(process.cwd(), headerPath);
       p = path.normalize(p).replace(/[\\]/g, '/');
       loadHeader('file://' + p).then((data, err) => {
-        console.log('loadHeader ->', {p, data, err});
         if (err) {
           p = `${appPath}/header.config.mjs`;
           p = path.normalize(p).replace(/[\\]/g, '/');
           loadHeader('file://' + p).then((data2, err2) => {
-            console.log('loadHeader ->', {p, data2, err2});
             if (err2) {
-              console.log(`Unable to prepend, because no valid header javascript file in app root or passed as path. Argument passed: ${headerPath}`);
-              //process.exit(1);
               reject(err);   // reject with original error
             } else {
               resolve(data2);
             }
           }).catch((err) => {
-            console.error('headerPromise error', err)
             reject(err);
-          }).finally(() => {
-            console.error('headerPromise final.')
           });
         } else {
           resolve(data);
         }
       }).catch((err) => {
-        console.error('headerPromise error', err)
         reject(err);
-      }).finally(() => {
-        console.error('headerPromise final.')
       });
     }
   });
@@ -117,58 +97,44 @@ function importHeaderJS(headerPath) {
 let headerPromise = importHeaderJS(headerPath);
 
 const prependHeader = (filePath, text) => {
-  console.log('prependHeader...')
   return new Promise((resolve, reject) => {
     // grab headerTxt from global
-    console.log('prependFile...')
-    prependFile(filePath, text, (data, err) => {
-      console.log('prependFile ->', {data, err})
+    prependFile(filePath, text).then((data, err) => {
       if (err) {
-        console.log('Could not prepend header.');
         reject(err);
+      } else {
+        resolve();
       }
-      console.log(`repended to ${filePath}`);
-      resolve();
+    }).catch((err) => {
+      console.error('prependHeader error', err)
     });
   });
 };
 
 const conditionallyReadAndPrependHeaderToFile = filePath => {
-console.log('conditionallyReadAndPrependHeaderToFile', filePath);
 
   readFilePromise(filePath).then(output => {
     // don't accidentally double-append.
-console.log('output....', output);
-    console.log('before headerPromise...')
     headerPromise.then((hdata, herr) => {
-      console.log('headerPromise', {hdata, herr})
-
       let header = hdata.default;
 
       if (!output.includes(header.match)) {
-        console.log(`appending header to ${filePath}.)`)
         prependHeader(filePath, header.text).then((d, e) => {
-          console.log('prependHeader ->', {d, e})
+          console.log('Done.');
         }).catch((err) => {
+          console.log('Could not prepend header.');
           console.error('prependHeader error', err)
-        }).finally(() => {
-          console.error('prependHeader final.')
         });
       } else {
         console.log(`(Header already exists in ${filePath}.)`)
       }
     }).catch((err) => {
+      console.log(`Unable to prepend, because no valid header javascript file in app root or passed as path. Argument passed: ${headerPath}`);
       console.error('headerPromise error', err)
-    }).finally(() => {
-      console.error('headerPromise final.')
     });
   }).catch((err) => {
     console.error('readFilePromise error', err)
-  }).finally(() => {
-    console.error('readFilePromise final.')
   });
-
-console.log('conditionallyReadAndPrependHeaderToFile END');
 };
 
 // all this complex logic to basically check if user is passing a glob (e.g. [src-web/**/*.js])
--- utils.mjs
+++ utils.mjs
@@ -5,18 +5,14 @@ export function flatten(arr) {
 }
 
 export function readFilePromise(filePath) {
-  console.log('readFilePromise', filePath)
   return new Promise((resolve, reject) => {
-    console.log('readFile', filePath)
     try {
       fs.readFile(filePath, 'utf8', (err, data) => {
-        console.log('readFile:', {data, err})
         if (err) reject(err);
         resolve(data);
       });
     } catch (e) {
-      console.log(e)
+      reject(e);
     }
-    console.log('...')
   });
 }
